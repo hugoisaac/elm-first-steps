2be3c9eaaeada332120f39102f37a0070bd06840
Hugo Ortiz <hugo.isaac@gmail.com>
2016-05-18 19:12:07 -0700

    Better implementation of Add, Update, and Reset.

--- main.elm
+++ main.elm
@@ -1,7 +1,8 @@
 import Html exposing (..)
 import Html.App as Html
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, onInput)
+import String exposing (..)
 
 main =
   Html.beginnerProgram { model = model, view = view, update = update }
@@ -10,7 +11,7 @@ main =
 
 type alias Model = {
     warriors : List Warrior
-    , latestId : Int
+    , newWarrior : Warrior
 }
 
 type alias Warrior = {
@@ -18,25 +19,25 @@ type alias Warrior = {
     , firstName : String
     , lastName : String
     , ki: Int
-    , vegetaSays : String
+    , vegetaSays : String   
 }
 
 type alias Id = Int
 
 model : Model
-model = Model warriors (Maybe.withDefault 0 (List.maximum (List.map (\w -> w.id) warriors)))
+model = Model warriors newWarrior
 
 warriors : List Warrior       
 warriors = [pepe, juan, max, goku]
 
-defaultWarrior : Warrior
-defaultWarrior = createWarrior 0 "" "" 0
+newWarrior : Warrior
+newWarrior = createNewWarrior warriors
 
 pepe : Warrior
 pepe = createWarrior 1 "Pepe" "Pecas" 365
 
 juan : Warrior
-juan = createWarrior 2 "Juan" "Jones" 299
+juan = createWarrior 2 "Juan" "Solo" 299
 
 max : Warrior
 max = createWarrior 3 "Max" "Power" 999
@@ -47,6 +48,14 @@ goku = createWarrior 4 "Goku" "" 1196
 createWarrior : Int -> String -> String -> Int -> Warrior
 createWarrior id firstName lastName ki =
     Warrior id firstName lastName ki (powerLevel ki)
+
+createNewWarrior : List Warrior -> Warrior
+createNewWarrior warriors =
+    createWarrior (getNewWarriorId warriors) "" "" 0
+
+getNewWarriorId : List Warrior -> Id
+getNewWarriorId warriors = 
+    Maybe.withDefault 1 (List.maximum (List.map (\w -> w.id) warriors)) + 1
 
 powerLevel : Int -> String
 powerLevel ki =
@@ -62,7 +71,13 @@ boostKi ki =
 -- UPDATE
 
 -- TODO: Add, Remove Id messages.
-type Msg = UseKaioken Id
+type Msg = 
+    UseKaioken Id 
+    | UpdateFirstName Id String 
+    | UpdateLastName Id String 
+    | UpdateKi Id String 
+    | Add
+    | Reset Id
 
 update : Msg -> Model -> Model
 update msg model =
@@ -74,38 +89,112 @@ update msg model =
                     if warrior.id == id 
                     then 
                         { warrior 
-                          | ki = boostKi warrior.ki
-                          , vegetaSays = powerLevel (boostKi warrior.ki)                         
+                        | ki = boostKi warrior.ki
+                        , vegetaSays = powerLevel (boostKi warrior.ki)                         
                         } 
                     else warrior
             in                
                 { model
-                  | warriors = List.map boostWarrior model.warriors
+                | warriors = List.map boostWarrior model.warriors
                 }
+        UpdateFirstName id firstName ->
+            let 
+                updateFirstName : WarriorAction
+                updateFirstName warrior =
+                    transform warrior id (\w -> { w | firstName = firstName })
+            in
+                { model
+                | warriors = List.map updateFirstName model.warriors
+                , newWarrior = updateFirstName model.newWarrior
+                }
+        UpdateLastName id lastName ->
+            let 
+                updateLastName : Warrior -> Warrior
+                updateLastName warrior =
+                    transform warrior id (\w -> { w | lastName = lastName })
+            in
+                { model
+                | warriors = List.map updateLastName model.warriors
+                , newWarrior = updateLastName model.newWarrior
+                }
+        UpdateKi id ki ->
+            let 
+                updateKi : Warrior -> Warrior
+                updateKi warrior =
+                    transform warrior id (\w -> { w | ki = Result.withDefault 0 (String.toInt ki) })
+            in
+                { model
+                | warriors = List.map updateKi model.warriors
+                , newWarrior = updateKi model.newWarrior
+                }
+        Add ->
+            { model
+            | warriors = model.warriors ++ [model.newWarrior]
+            , newWarrior = reset model.newWarrior |> bumpId
+            }
+        Reset id ->
+            { model
+            | warriors = List.map (\w -> transform w id reset) model.warriors
+            , newWarrior = (\w -> transform w id reset) model.newWarrior
+            }
 
+type alias WarriorAction = Warrior -> Warrior
+
+reset : WarriorAction
+reset warrior =
+    { warrior
+    | firstName = ""
+    , lastName = ""
+    , ki = 0
+    }
+
+bumpId : WarriorAction
+bumpId warrior = 
+    { warrior
+    | id = warrior.id + 1
+    }
+    
+transform : Warrior -> Id -> (WarriorAction) -> Warrior
+transform warrior id action  =
+    if warrior.id == id then (action warrior) else warrior
+        
 -- VIEW
 
 view : Model -> Html Msg
 view model =
     div [] 
     [ h1 [] [text "Warriors"]
-    , addwarriorView
-    
+    , addwarriorView model.newWarrior   
     , table [] (warriorHeaderView :: (List.map warriorRowView model.warriors))            
     , br [] []
     -- , modelView model
     ]
 
-addwarriorView : Html Msg
-addwarriorView =
+addwarriorView : Warrior -> Html Msg
+addwarriorView warrior =
     div []
     [ label [ for "firstName" ] [ text "First Name:" ]
-    , input [ type' "text", id "firstName" ] []
+    , input 
+        [ type' "text"
+        , id "firstName"
+        , value warrior.firstName
+        , onInput (UpdateFirstName warrior.id) ] []
     , label [ for "lastName" ] [ text "Last Name:" ]
-    , input [ type' "text", id "lastName" ] []
+    , input 
+        [ type' "text"
+        , id "lastName"
+        , value warrior.lastName
+        , onInput (UpdateLastName warrior.id) ] []
     , label [ for "ki" ] [ text "Ki:" ]
-    , input [ type' "text", id "ki" ] []
-    , input [ type' "button", value "Add" ] [] 
+    , input 
+        [ type' "text"
+        , id "ki"
+        , value (getNewWarriorKi warrior.ki)
+        , onInput (UpdateKi warrior.id) ] []
+    , input 
+        [ type' "button"
+        , value "Add"
+        , onClick Add ] []
     ]    
 
 warriorHeaderView : Html Msg
@@ -128,6 +217,10 @@ warriorRowView warrior =
     , td [] [button [onClick (UseKaioken warrior.id)] [text "Kaio-ken!"]] 
     ]
     
-modelView : Model -> Html Msg
-modelView model =
-    div [] [ text ("model: " ++ (toString model)) ]   
+-- modelView : Model -> Html Msg
+-- modelView model =
+--     div [] [ text ("model: " ++ (toString model)) ]
+    
+getNewWarriorKi : Int -> String
+getNewWarriorKi ki =
+    if ki == 0 then "" else toString ki 
